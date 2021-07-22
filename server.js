59fcbd9737ee77cbc5c839ec135e304da1b434ad
Chay Williams <Chayvw@gmail.com>
2021-07-22 10:54:40 -0400

    added static client build

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("client/build"));
+
 app.get("/api/config", (req, res) =>{
     res.json({success:true})
 });
