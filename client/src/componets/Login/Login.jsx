2f44a532d4fc68e1d0f90d7e86a7f94216bbd102
Chay Williams <Chayvw@gmail.com>
2021-07-26 10:26:50 -0400

    created signup input capture with hooks

--- client/src/componets/Login/Login.jsx
+++ client/src/componets/Login/Login.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login(props) {
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+
     return (
-        <div>
-           <h4>Login Page</h4> 
+        <div class="row">
+            <form class="col s4">
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="password" type="password" class="validate" value={password} onChange={(e) =>{setPassword(e.target.value)}}/>
+                        <label for="password">Password</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="email" type="email" class="validate" value={email} onChange={(e) =>{setEmail(e.target.value)}} />
+                        <label for="email">Email</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="email" type="text" class="validate" value={name} onChange={(e) =>{setName(e.target.value)}} />
+                        <label for="name">Name</label>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 }
