76a3e6f4b2a8ed31b690137d8571c0f09b79536c
Chay Williams <Chayvw@gmail.com>
2021-07-26 20:15:14 -0400

    axios call so I user can sing up

--- client/src/componets/Login/Login.jsx
+++ client/src/componets/Login/Login.jsx
@@ -1,32 +1,54 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
-function Login(props) {
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
+  function Login(props){
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // axios call to login
+        axios
+            .post("/api/user", { email: email, password: password, name: name })
+            .then((response) => {
+                console.log(response.data);
+                // const loginHandle =(response.data.data);
+
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        console.log("Working")
+    };
 
     return (
-        <div class="row">
-            <form class="col s4">
-                <div class="row">
-                    <div class="input-field col s12">
-                        <input id="password" type="password" class="validate" value={password} onChange={(e) =>{setPassword(e.target.value)}}/>
-                        <label for="password">Password</label>
+        <div className="row">
+            <form className="col s4" onSubmit={handleSubmit}>
+                <div className="row">
+                    <div className="input-field col s12">
+                        <input id="password" type="password" className="validate" value={password} onChange={(e) =>{setPassword(e.target.value)}}/>
+                        <label htmlFor="password">Password</label>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="input-field col s12">
-                        <input id="email" type="email" class="validate" value={email} onChange={(e) =>{setEmail(e.target.value)}} />
-                        <label for="email">Email</label>
+                <div className="row">
+                    <div className="input-field col s12">
+                        <input id="email" type="email" className="validate" value={email} onChange={(e) =>{setEmail(e.target.value)}} />
+                        <label htmlFor="email">Email</label>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="input-field col s12">
-                        <input id="email" type="text" class="validate" value={name} onChange={(e) =>{setName(e.target.value)}} />
-                        <label for="name">Name</label>
+                <div className="row">
+                    <div className="input-field col s12">
+                        <input id="email" type="text" className="validate" value={name} onChange={(e) =>{setName(e.target.value)}} />
+                        <label htmlFor="name">Name</label>
                     </div>
                 </div>
+                <div className="row">
+                    <div className="col s4">
+                        <button className="btn waves-effect waves-light" type="submit" name="action">SignUP
+                        </button>
+                        </div>
+                        </div>
             </form>
         </div>
     );
